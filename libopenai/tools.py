f178a6e0837abb4ee5d072c631d6035bb8ccf203
marco <marcotts@gmail.com>
2023-10-19 15:10:26 +0200

    update

--- libopenai/tools.py
+++ libopenai/tools.py
@@ -32,7 +32,6 @@ def generate_example_text_slo_ita(word_list, OPENAI_API_KEY=None, temperature=0.
 
     {
       "words_list": ["tekmovanja"],
-      "number_of_sentences": 2,
       "slovenian_text": "Naša šola je organizirala veliko športnih tekmovanj. Tudi letos bomo imeli tekmovanje v teku, kjer se lahko vsi učenci pomerijo.",
       "italian_translation": "La nostra scuola ha organizzato molte competizioni sportive. Anche quest'anno avremo una gara di corsa, in cui tutti gli studenti possono competere."
     }
@@ -43,29 +42,12 @@ def generate_example_text_slo_ita(word_list, OPENAI_API_KEY=None, temperature=0.
 
     openai.api_key = OPENAI_API_KEY
 
-    # system_content = (
-    #     "the input is a list of slovenian words (one or more words; N is the number of slovenian words), where each word is included in apexes. "
-    #     "You have to generate a random text (composed by one or more sentences) which must include, at least once, all the input words passed as role 'user'."
-    #     " The output must be valid JSON and must include: the original input words (parameter name: words_list), the generated text in slovenian language,"
-    #     "and the correct italian translation of the same text. "
-    #     "Also, in the JSON output, you must include the number of sentences generated in the slovenian text (parameter name: number_of_sentences)."
-    #     )
-
     system_content = (
         "the input is a list of slovenian words, where each word is included in apexes. "
         "You have to generate a random text which must include all the input words passed as role 'user'. Try to minimize the number of sentences generated."
         " The output must be valid JSON and must include: the original input words (parameter name: words_list), the generated text in slovenian language (parameter name: slovenian_text,"
         "and the correct italian translation of the same text (parameter name: 'italian_text'). "
         )
-
-    # system_content = ("the input is a list of slovenian words (one or more words; N is the number of slovenian words), where each word is included in apexes. "
-    #                   "You have to generate a random text (composed by one or more sentences) which must include, at least once, all the input words passed as role 'user'."
-    #                   " The output must be valid JSON and must include: the original input words (parameter name: words_list), the generated text in slovenian language,"
-    #                   "and the correct italian translation of the same text. "
-    #                   "Also, in the JSON output, you must include the number of sentences generated in the slovenian text (parameter name: number_of_sentences)."
-    #                   "Also, in the JSON output, you must include other N-1 italian texts (grouped in a list called 'wrong_ita_answers')"
-    #                   " which are random i.e not even correlated to the 'correct' translation of the generated slovene text."
-    #                   )
 
     str_word_list = ", ".join([f"'{word}'" for word in word_list])
 
@@ -88,8 +70,6 @@ def generate_example_text_slo_ita(word_list, OPENAI_API_KEY=None, temperature=0.
         presence_penalty=0
     )
 
-    # print(response)
-
     try:
         data_str = response.choices[0].message.content
     except Exception:
@@ -103,16 +83,8 @@ def generate_example_text_slo_ita(word_list, OPENAI_API_KEY=None, temperature=0.
     return data_json, data_str, response
 
 
-
 if __name__ == '__main__':
-    # word_list = ["tekmovanja", "šola", "tek", "učenci", "pomerijo"]
-    # word_list = ["tekmovanja", "šola", "tek", "učenci", "pomerijo", "tekmovalci",
-    #                 "tekmovanje", "šport", "športni", "športna", "športno", "športne",
-    #                 "športnik", "športniki", "športnica", "športnice", "športnico", "športnic",
-    #                 "športnikov", "športnikom", "športnikih", "športniku", "športniki",
-    #                 "športnikov", "športnikom", "športnikih", "športniku", "športniki",
-    #              ]
-
+    # this is just a test
     word_list = ["tekmovanja", "šola", ]
 
     data_json, data_str, response = generate_example_text_slo_ita(word_list)
