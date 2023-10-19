e11e3a01b0a55db62cbe414bda872e41b696fff6
marco <marcotts@gmail.com>
2023-10-19 17:01:17 +0200

    update

--- sentence_generator/settings.py
+++ sentence_generator/settings.py
@@ -17,6 +17,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+
+STATIC_URL = '/static/'
+
 # Set up django-environ
 env = environ.Env(
     # Default values for variables if not set
@@ -128,12 +134,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
