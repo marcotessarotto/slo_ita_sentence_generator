28f593715171c5aa0ee000fce9e887a6029347b8
marco <marcotts@gmail.com>
2023-10-20 11:43:21 +0200

    update

--- core/management/commands/GetExampleSentence.py
+++ core/management/commands/GetExampleSentence.py
@@ -5,22 +5,34 @@ from libopenai.tools import generate_example_text_slo_ita
 
 
 class Command(BaseCommand):
-    help = "Receives a list of words as arguments."
+    help = "Receives a list of words as arguments. Creates a WordListWithSampleTextAndTranslation instance."
 
     def add_arguments(self, parser):
         # Add a positional argument for the words. nargs='+' allows for multiple arguments.
         parser.add_argument('words', nargs='+', type=str, help='List of words to process')
 
+        # add an optional integer argument
+        parser.add_argument(
+            '--max_num_examples_per_word_list',
+            action='store',
+            dest='max_num_examples_per_word_list',
+            default=1,
+            type=int,
+            help='Maximum number of examples per word list'
+        )
+
     def handle(self, *args, **kwargs):
         words = kwargs['words']
+
+        max_num_examples_per_word_list = kwargs['max_num_examples_per_word_list']
 
         # Do something with the words. For this example, we'll just print them.
         self.stdout.write(self.style.SUCCESS('Received the following words:'))
         for word in words:
             self.stdout.write(word)
 
-        if get_number_of_examples(words, language='slovenian') > 1:
-            self.stdout.write(self.style.ERROR('There are already examples for the provided words'))
+        if (num := get_number_of_examples(words, language='slovenian')) > max_num_examples_per_word_list:
+            self.stdout.write(self.style.ERROR(f'There are already {num} examples for the provided words'))
             return
 
         data_json, data_str, response = generate_example_text_slo_ita(list(words))
